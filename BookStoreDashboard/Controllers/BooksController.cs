8ad477606b9d0baaaf5f2f0ab31c447e95005200
tnakevski@outlook.com <tnakevski@outlook.com>
2020-07-14 13:17:51 +0200

    implemented admin dashboard for our BookStore API

---
+++ BookStoreDashboard/Controllers/BooksController.cs
@@ -0,0 +1,74 @@
+﻿using BookStoreDashboard.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BookStoreDashboard.Controllers
+{
+    public class BooksController : Controller
+    {
+        public async Task<IActionResult> Overview()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            var httpResponse = await httpClient.GetAsync("https://localhost:44388/api/books");
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                var response = await httpResponse.Content.ReadAsStringAsync();
+                var books = JsonConvert.DeserializeObject<List<BookViewModel>>(response);
+                return View(books);
+            }
+
+
+            return RedirectToAction("Error", "Home");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            var httpResponse = await httpClient.GetAsync($"https://localhost:44388/api/books/{id}");
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                var response = await httpResponse.Content.ReadAsStringAsync();
+                var book = JsonConvert.DeserializeObject<BookViewModel>(response);
+                return View(book);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(BookViewModel book)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            var httpResponse = await httpClient.PutAsJsonAsync<BookViewModel>($"https://localhost:44388/api/books", book);
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                ViewBag.SuccessMessage = "Updated successfully";
+                return View();
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Update failed";
+                return View();
+            }
+
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            HttpClient _client = new HttpClient();
+
+            var response = await _client.DeleteAsync($"https://localhost:44388/api/Books/{id}");
+
+            return RedirectToAction(nameof(Overview));
+        }
+    }
+}
---
+++ BookStoreDashboard/Models/BookViewModel.cs
@@ -0,0 +1,23 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreDashboard.Models
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+
+    }
+}
